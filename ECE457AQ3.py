cad2558283e68fe96867f5e724a921fb1543cc2a
nihitgupta2 <118135114+nihitgupta2@users.noreply.github.com>
2022-12-28 01:23:05 -0500

    Add files via upload

---
+++ ECE457AQ3.py
@@ -0,0 +1,74 @@
+import random
+
+def main():
+    totalspots = [25,25]
+    maze = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+        [1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0]]
+    # I have assumed that in the given map on Learn the 0,0 is the bottom left square and the square 
+    # on its right is 1,0 and the square above 0,0 is 0,1 and so on 
+    # However in the above 2D arrays every sublist is a row and first sublist is the bottom most row
+    # so in order to access 9,11 graph value I use 11,9 in 2D Array.
+    # The coordinate values in output corresponds to the GRAPH. 
+    S = [2,11]      #start position
+    E1 = [23,19]    #Exit position 1 
+    E2 = [2,21]     #Exit position 2
+    n=100000 #total moves
+    currPos = S # current position of robot
+    prevPos = None # previos position of robot
+    moveCounter = 1 # total no. of moves which is the cost (in the assignment doc it mentions being at first box is cost 1)
+    allPositionData = []
+    while currPos!=E1 and currPos!=E2 and moveCounter<n:
+        print(currPos)
+        nextPosOpt = [[currPos[0]+1,currPos[1]],[currPos[0]-1,currPos[1]],[currPos[0],currPos[1]+1],[currPos[0],currPos[1]-1]]  # Total options
+        if prevPos in nextPosOpt:  # as we cannot go back to from where we came
+            nextPosOpt.remove(prevPos)
+        posRemove = []             # pos that have to be removed from total viable future options
+        for i in nextPosOpt:
+            if i[0]>=0 and i[0]<25 and i[1]>=0 and i[1]<25:   #checking for out of bound
+                if maze[i[1]][i[0]] == 0:                     # 0 is not blocked box
+                    pass
+                else:
+                    posRemove.append(i)
+            else:
+                posRemove.append(i)
+        for i in posRemove:
+            nextPosOpt.remove(i)
+        print(nextPosOpt)
+        if(nextPosOpt!=[]):
+            nextPos = random.choice(nextPosOpt)
+            moveCounter+=1
+            prevPos = currPos
+            currPos = nextPos
+            allPositionData.append(currPos)
+        else:
+            print("Deadend")   #this means the robot is in a spot from where there are no viable options to move to
+            break
+
+    print(allPositionData)
+    print(f'total moves / cost = {moveCounter}')
+
+if __name__ == '__main__':
+    main()
